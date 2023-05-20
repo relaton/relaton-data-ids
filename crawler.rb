2a51db6ccb196a8385577765d2e0bf751359c63c
Andrei Kislichenko <android.2net@gmail.com>
2023-05-19 23:02:06 -0400

    create index

--- crawler.rb
+++ crawler.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 require 'fileutils'
 
 system("rsync -avcizxL rsync.ietf.org::bibxml-ids ./bibxml-ids")
 
 require "relaton_ietf"
+
+FileUtils.rm Dir.glob("index-*")
+FileUtils.rm_rf "data"
+
 RelatonIetf::DataFetcher.fetch "ietf-internet-drafts"
+
+system("zip index-v1.zip index-v1.yaml")
+system("git add index-v1.zip, index-v1.yaml")
