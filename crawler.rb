04fc066dd4995c4353ae69b9005c2e25bdd845d7
Andrei Kislichenko <android.2net@gmail.com>
2023-06-07 16:03:42 -0400

    Move index deleting to the corresponding crawler.rb scripts

--- crawler.rb
+++ crawler.rb
@@ -6,10 +6,7 @@ system("rsync -avcizxL rsync.ietf.org::bibxml-ids ./bibxml-ids")
 
 require "relaton_ietf"
 
-FileUtils.rm Dir.glob("index-*")
+FileUtils.rm Dir.glob("index*")
 FileUtils.rm_rf "data"
 
 RelatonIetf::DataFetcher.fetch "ietf-internet-drafts"
-
-system("zip index-v1.zip index-v1.yaml")
-system("git add index-v1.zip index-v1.yaml")
